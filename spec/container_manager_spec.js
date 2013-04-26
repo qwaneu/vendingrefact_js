b4798ab573d3b68926482c004a0a46797314327d
Willem van den Ende <willem@livingsoftware.nl>
2013-04-26 17:04:30 +0100

    can manager (drawers) class to mimic java Map, for easier porting
    also makes for somewhat obfuscated use of object as hash. might inline
    later.

---
+++ spec/container_manager_spec.js
@@ -0,0 +1,22 @@
+"use strict";
+
+var buster = require("buster");
+
+var misc = require("../lib/can");
+
+buster.spec.expose(); //expose spec functions describe() and it()
+
+describe("ContainerManager", function () {
+
+	it("gets what it puts", function() {
+		var manager = new misc.ContainerManager();
+		manager.put("key", "the value");
+		expect("the value").toEqual(manager.get("key"));
+	});
+
+	it("has not what it does not put", function() {
+		var manager = new misc.ContainerManager();
+		expect(false).toEqual(manager.containsKey("key"));
+	});
+
+});
